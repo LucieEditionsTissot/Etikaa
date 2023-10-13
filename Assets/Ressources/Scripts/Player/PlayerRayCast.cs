4bdeac8110f688d732c5afe6ebe79cd27f1c900f
Joyce <lapinette2002@ymail.com>
2023-10-13 13:26:09 +0200

    Création du système d'interactions

---
+++ Assets/Ressources/Scripts/Player/PlayerRayCast.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using UnityEngine;
+
+public class PlayerRayCast : MonoBehaviour
+{
+    [SerializeField] float rayDistance = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+
+        //shoot a raycast where the player is looking
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0)), out hit, rayDistance))
+        {
+            Debug.DrawRay(Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0)).origin, Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0)).direction * rayDistance, Color.red);
+            Debug.Log(hit.collider.gameObject.name);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if(hit.collider.gameObject.tag == "Interactable")
+                {
+                    hit.collider.gameObject.GetComponent<Interactable>()?.Interact();
+                }
+            }
+        }
+        
+    }
+}
