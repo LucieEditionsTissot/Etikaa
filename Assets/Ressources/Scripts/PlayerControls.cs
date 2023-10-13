d051062d8e936dead1a70056ff99e610705ab11b
Joyce <lapinette2002@ymail.com>
2023-10-13 11:22:36 +0200

    Fix camera + déplacement

--- Assets/Ressources/Scripts/PlayerControls.cs
+++ Assets/Ressources/Scripts/PlayerControls.cs
@@ -7,42 +7,40 @@ public class PlayerControls : MonoBehaviour
     [SerializeField] Rigidbody rb;
     [SerializeField] float forwardForce = 0f;
     [SerializeField] float jumpForce = 10f;
+    [SerializeField] Camera cam;
 
-    private bool zPressed;
-    private bool sPressed;
-    private bool qPressed;
-    private bool dPressed;
+    //mouse sensitivity
+    [SerializeField] float mouseSensitivity = 1f;
     private bool spacePressed;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //rotate the player with the mouse
-        float mouseX = Input.GetAxis("Mouse X");
-        float mouseY = Input.GetAxis("Mouse Y");
-        transform.Rotate(0, mouseX, 0);
-    }
-
-    private void FixedUpdate()
-    {
+        if(Time.frameCount < 10){
+            return;
+        }
         //ZQSD player controls
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
+        
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+        transform.Rotate(0, mouseX * mouseSensitivity, 0);
+        
+        //80 0 360 280
+        float nextCamRotation = cam.transform.localRotation.eulerAngles.x + (-mouseY * Time.deltaTime * mouseSensitivity);
+        if((nextCamRotation < 80 && nextCamRotation > 0) || (nextCamRotation > 280 && nextCamRotation < 360))
+            cam.transform.RotateAround(cam.transform.right, -mouseY * Time.deltaTime * mouseSensitivity);
 
-        Debug.Log(moveHorizontal);
-        Debug.Log(moveVertical);
+        if(cam.transform.localRotation.eulerAngles.x > 80 && cam.transform.localRotation.eulerAngles.x < 180)
+            cam.transform.localRotation = Quaternion.Euler(80, cam.transform.localRotation.eulerAngles.y, cam.transform.localRotation.eulerAngles.z);
+        else if(cam.transform.localRotation.eulerAngles.x < 280 && cam.transform.localRotation.eulerAngles.x > 80)
+            cam.transform.localRotation = Quaternion.Euler(280, cam.transform.localRotation.eulerAngles.y, cam.transform.localRotation.eulerAngles.z);
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        movement = Camera.main.transform.TransformDirection(movement);
-        movement.y = 0.0f;
+        Vector3 direction = cam.transform.TransformDirection(movement);
+        direction.y = 0.0f;
 
-        transform.GetComponent<Rigidbody>().AddForce(movement * forwardForce * Time.deltaTime, ForceMode.VelocityChange);
+        transform.GetComponent<Rigidbody>().AddForce(direction * forwardForce * Time.deltaTime, ForceMode.VelocityChange);
 
         //Jump
         if (spacePressed)
